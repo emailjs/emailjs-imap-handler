407dc1cb2b17be12c164eb4e1d6761650e9984a5
Felix Hammerl <mail@felixhammerl.com>
2014-03-24 17:48:04 +0100

    change copyright headers to proper MIT

--- src/imap-compiler.js
+++ src/imap-compiler.js
@@ -6,25 +6,27 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 (function(root, factory) {
     "use strict";
 
-    // UMD shim, see: https://github.com/umdjs/umd/blob/master/returnExports.js
     if (typeof define === "function" && define.amd) {
-        define(["imap-formal-syntax"], factory); // amd
+        define(["imap-formal-syntax"], factory);
     } else if (typeof exports === "object") {
-        module.exports = factory(require("./imap-formal-syntax")); // common js
+        module.exports = factory(require("./imap-formal-syntax"));
     } else {
-        root.imapCompiler = factory(); // global
+        root.imapCompiler = factory();
     }
 }(this, function(imapFormalSyntax) {
 
