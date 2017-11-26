bf223f4184abdbcaa8c2b7ee937f255e0462c60a
Felix Hammerl <fhammerl@thoughtworks.com>
2017-11-26 14:41:37 +0100

    Updating dist files

---
+++ dist/handler.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _compiler = require('./compiler');
+
+Object.defineProperty(exports, 'compiler', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_compiler).default;
+  }
+});
+
+var _parser = require('./parser');
+
+Object.defineProperty(exports, 'parser', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_parser).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9oYW5kbGVyLmpzIl0sIm5hbWVzIjpbImRlZmF1bHQiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7OzZDQUFTQSxPOzs7Ozs7Ozs7MkNBQ0FBLE8iLCJmaWxlIjoiaGFuZGxlci5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCB7IGRlZmF1bHQgYXMgY29tcGlsZXIgfSBmcm9tICcuL2NvbXBpbGVyJ1xuZXhwb3J0IHsgZGVmYXVsdCBhcyBwYXJzZXIgfSBmcm9tICcuL3BhcnNlcidcbiJdfQ==
